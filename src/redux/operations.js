5bae5759dc6784f11e5e25df1c6f208714460f87
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-06-22 02:32:42 +0600

    api change

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://64871b0fbeba6297278ff224.mockapi.io';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
