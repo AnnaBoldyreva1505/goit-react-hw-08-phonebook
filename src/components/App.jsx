e145255a78ff384a31d17a42b9dd2c2e80416033
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-06-22 20:27:54 +0600

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchContacts } from "redux/operations";
-import { selectError, selectIsLoading } from "redux/contacts/contact-selectors";
+import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/contacts/contact-selectors';
 import { Toaster } from 'react-hot-toast';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { ContactForm } from './ContactForm/ContactForm';
-
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -18,15 +18,16 @@ export const App = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <Toaster position="top-right" reverseOrder={true} />
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter/>
-      {isLoading && !error && <b>Request in progress...</b>}
-      <ContactList />
-    </div>
+    <>
+      <Routes>
+        <Toaster position="top-right" reverseOrder={true} />
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        {isLoading && !error && <b>Request in progress...</b>}
+        <ContactList />
+      </Routes>
+    </>
   );
 };
-
