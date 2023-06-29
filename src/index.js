1529b9ac9f64032d08c2915593d89c32f038870e
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-06-29 22:27:51 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,24 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-<<<<<<< Updated upstream
-      {/* <BrowserRouter > */}
-        <App />
-      {/* </BrowserRouter> */}
-=======
       <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
->>>>>>> Stashed changes
     </Provider>
   </React.StrictMode>
 );
